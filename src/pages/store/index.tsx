93c044da0d21d6c60aed4fff13fc9a8352d258ab
adorno-dev <adorno.dev@gmail.com>
2022-10-16 09:57:49 -0300

    Working on store UI

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -7,11 +7,16 @@ import { storeService } from "../../services"
 import './index.scss'
 
 export const Store = () => {
+    const currency = Intl.NumberFormat("en-US", {style: "currency", currency: "USD"});
     const [store, setStore] = useState<Models.Store | null>()
     const fetchData = useCallback(()=>{
         storeService.getUserStore()
                     .then(res => setStore(res.data))
     }, [])
+    const toDatetime = (timestamp: Date | undefined) => {
+        const date = new Date(timestamp?.toString() ?? "")
+        return date.toLocaleDateString("en-US", {year: 'numeric', month: 'long', day: '2-digit'})
+    }
     const deleteUserStore = async () => {
         store?.id && await storeService.deleteUserStore(store.id)
         setStore(null)
@@ -23,7 +28,7 @@ export const Store = () => {
     <Authorized>
     <Navbar />
     <Placeholder>
-    {
+    {/* {
         ! store ?
         <section className="store">
             <h2>Store Section</h2>
@@ -42,7 +47,63 @@ export const Store = () => {
             </div>
             <Link to="/store" onClick={deleteUserStore}>Delete this store</Link>
         </section>
-    }
+    } */}
+    
+        {
+            ! store ?
+            <section className="store">
+                <h2>You don't have a store yet.</h2>
+                <p>Would you like to create one?.</p>
+                <Link to="/store/new">[ Yes, please ]</Link>
+                <Link to="/">[ Not interessed ]</Link>
+            </section>
+            :
+            <div className="view-store">
+            <div className="hflex">
+                <div>
+                    <img src={store?.logo} />
+                    <div>
+                        <b>{store?.name}</b>
+                        <p>Sold by: {store?.user.username}</p>
+                        <p>Joined: {toDatetime(store?.joined)}</p>
+                    </div>
+                </div>
+                <img src={store?.banner} />
+            </div>       
+            <div className="hflex options">
+                <div>
+                    <h3>Options</h3>
+                    <Link to="/admin/products">New Product</Link>
+                    <Link to="/">Go to Marketplace</Link>
+                    {/* <Link to="/store" onClick={deleteUserStore}>Delete this store</Link> */}
+                </div>
+                <ul className="products">
+                {
+                    store?.products?.map(p => 
+                        <li key={p.id}>
+                            <Link to={`/products/${p.id}`}>
+                                <img src={p.screenshoot} />
+                            </Link>
+                            <Link className="product" to={`/products/${p.id}`}>{p.name}</Link>
+                            <div className="split">
+                                <span className="reviews">
+                                    <i className="fa-sharp fa-solid fa-star"></i>
+                                    <i className="fa-sharp fa-solid fa-star"></i>
+                                    <i className="fa-sharp fa-solid fa-star"></i>
+                                    <i className="fa-sharp fa-solid fa-star"></i>
+                                    <i className="fa-sharp fa-solid fa-star"></i>
+                                    <Link to="reviews">0 Reviews</Link>
+                                </span>
+                                <span className="price">{currency.format(p.price).replace("$", "$ ")}</span>
+                            </div>
+                        </li>
+                    )
+                }
+                </ul>
+                </div>
+            </div>
+        }
+
     </Placeholder>
     </Authorized>
     </>
